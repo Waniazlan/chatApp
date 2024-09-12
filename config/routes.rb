772aa1ebf14697fed5967fc4d94dcf6533833168
Waniazlan <waniazlan27@gmail.com>
2024-09-12 12:28:19 +0800

    new commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+    get '/signin', to: 'sessions#new'
+    post '/signin', to: 'sessions#create'
+    delete '/signout', to: 'sessions#destroy'
+
+    resources :rooms
+    resources :users
+    root 'rooms#index'
+    
+    resources :rooms do
+        resources :messages
+      end
+  end
